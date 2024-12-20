4786dced64c6bdefe484b34bf5ff6e3591f9231f
yrichi <yrichi59@gmail.com>
2024-12-20 23:06:06 +0100

    init kata

---
+++ src/main/java/com/kata/dictionaryreplacer/DictionaryReplacerApplication.java
@@ -0,0 +1,13 @@
+package com.kata.dictionaryreplacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryReplacerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryReplacerApplication.class, args);
+    }
+
+}
