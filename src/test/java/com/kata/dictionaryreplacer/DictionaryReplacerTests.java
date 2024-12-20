db3580c3937519c1a519e01293a9f85cdd8fec8a
yrichi <yrichi59@gmail.com>
2024-12-20 23:20:13 +0100

    second test with two entries

--- src/test/java/com/kata/dictionaryreplacer/DictionaryReplacerTests.java
+++ src/test/java/com/kata/dictionaryreplacer/DictionaryReplacerTests.java
@@ -22,4 +22,19 @@ public class DictionaryReplacerTests {
         Assertions.assertThat(result).isEqualTo("Hello John");
     }
 
+
+    @Test
+    void should_replace_two_words_when_dictionary_has_two_entries() {
+        // GIVEN
+        DictionaryReplacer dictionaryReplacer = new DictionaryReplacer();
+        String input = "Hello $name$ $surname$";
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("name", "John");
+        dictionary.put("surname", "Doe");
+        // WHEN
+        String result = dictionaryReplacer.replace(input, dictionary);
+        // THEN
+        Assertions.assertThat(result).isEqualTo("Hello John Doe");
+    }
+
 }
