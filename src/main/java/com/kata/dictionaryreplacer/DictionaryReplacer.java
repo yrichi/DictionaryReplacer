34e8da1d8e7ebc93038b7312ad2ab3c7a077ada7
yrichi <yrichi59@gmail.com>
2024-12-20 23:59:58 +0100

    refactor

--- src/main/java/com/kata/dictionaryreplacer/DictionaryReplacer.java
+++ src/main/java/com/kata/dictionaryreplacer/DictionaryReplacer.java
@@ -12,7 +12,7 @@ public class DictionaryReplacer {
         return dictionary.keySet().stream()
                 .reduce(input,
                         (acc, key) -> acc.replace(getFormattedKey(key,openSeparator,closeSeparator), dictionary.get(key)),
-                        (acc, key) -> acc);
+                        (acc, _) -> acc);
     }
 
     private static String getFormattedKey(String key, String openSeparator, String closeSeparator) {
